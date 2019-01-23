8295045929b239ab9543de3b9f6c7f12879e534f
Ryan Rusnak <rusnak@vt.edu>
2019-01-23 11:38:44 -0800

    adding files

---
+++ hello_world.py
@@ -0,0 +1,122 @@
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import numpy as np
+import pandas as pd
+import os
+import time
+import functools
+
+text = ' '.join(['Hello World' for n in range(1000)])
+vocab = sorted(set(text))
+
+# Creating a mapping from unique characters to indices
+char2idx = {u:i for i, u in enumerate(vocab)}
+idx2char = np.array(vocab)
+text_as_int = np.array([char2idx[c] for c in text])
+
+# The maximum length sentence we want for a single input in characters
+seq_length = 11
+examples_per_epoch = len(text)//seq_length
+
+# Create training examples / targets
+char_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
+sequences = char_dataset.batch(seq_length+1, drop_remainder=True)
+
+def split_input_target(chunk):
+    input_text = chunk[:-1]
+    target_text = chunk[1:]
+    return input_text, target_text
+
+dataset = sequences.map(split_input_target)
+
+# Batch size
+BATCH_SIZE = 1
+steps_per_epoch = examples_per_epoch//BATCH_SIZE
+BUFFER_SIZE = 10000
+
+dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
+
+# Length of the vocabulary in chars
+vocab_size = len(vocab)
+# The embedding dimension
+embedding_dim = 8
+# Number of RNN units
+rnn_units = 8
+
+rnn = functools.partial(tf.keras.layers.GRU, recurrent_activation='sigmoid')
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                              batch_input_shape=[batch_size, None]),
+    rnn(rnn_units,
+        return_sequences=True,
+        recurrent_initializer='glorot_uniform',
+        stateful=True),
+    rnn(rnn_units,
+        return_sequences=True,
+        recurrent_initializer='glorot_uniform',
+        stateful=True),
+    tf.keras.layers.Dense(vocab_size)
+  ])
+  return model
+
+model = build_model(
+  vocab_size = len(vocab),
+  embedding_dim=embedding_dim,
+  rnn_units=rnn_units,
+    batch_size=BATCH_SIZE)
+
+def loss(labels, logits):
+  return tf.keras.backend.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+model.compile(
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=2.0),
+    loss = loss)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+checkpoint_callback=tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+EPOCHS=3
+history = model.fit(dataset.repeat(), epochs=EPOCHS, steps_per_epoch=steps_per_epoch, callbacks=[checkpoint_callback])
+
+tf.train.latest_checkpoint(checkpoint_dir)
+
+model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
+model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
+model.build(tf.TensorShape([1, None]))
+
+def hello_world(model, start_string):
+  # Number of characters to generate
+  num_generate = 10
+
+  # Converting our start string to numbers (vectorizing)
+  input_eval = [char2idx[s] for s in start_string]
+  input_eval = tf.expand_dims(input_eval, 0)
+
+  # Empty string to store our results
+  text_generated = []
+  temperature = 1.0
+
+  # Here batch size == 1
+  model.reset_states()
+  for i in range(num_generate):
+      predictions = model(input_eval)
+      # remove the batch dimension
+      predictions = tf.squeeze(predictions, 0)
+
+      # using a multinomial distribution to predict the word returned by the model
+      predictions = predictions / temperature
+      predicted_id = tf.multinomial(predictions, num_samples=1)[-1,0].numpy()
+      # We pass the predicted word as the next input to the model along with the previous hidden state
+      input_eval = tf.expand_dims([predicted_id], 0)
+      text_generated.append(idx2char[predicted_id])
+
+  return (start_string + ''.join(text_generated))
+
+print(hello_world(model, start_string="H"))
+# model.save('saved_model.h5')
